ddc9881f1ad61503ce0d1c070423cda22e0ae768
abnepc <annie.nguyen@epcpower.com>
2024-04-16 13:45:07 -0700

    style(SC-1929): Reformatted two files using `black`

--- src/epcpm/parameterstointerface.py
+++ src/epcpm/parameterstointerface.py
@@ -1001,11 +1001,8 @@ def can_getter_setter_variable(can_signal, parameter, var_or_func_or_table):
             f"{can_signal.tree_parent.name}"
             f".{can_signal.name}"
         )
-    elif can_signal.tree_parent.tree_parent.name == 'CAN':
-        can_variable = (
-            f"&{can_signal.tree_parent.name}"
-            f".{can_signal.name}"
-        )
+    elif can_signal.tree_parent.tree_parent.name == "CAN":
+        can_variable = f"&{can_signal.tree_parent.name}" f".{can_signal.name}"
     else:
         can_variable = (
             f"&{can_signal.tree_parent.tree_parent.name}"
