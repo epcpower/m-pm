bf57c0572a13db3289b7e7e03ad9e32035be8589
David Michaud <david.michaud@epcpower.com>
2025-03-19 16:11:29 -0400

    fix: turns out, it's okay to have parameters multiply associated, KTHX

--- src/mpm/parameterstointerface.py
+++ src/mpm/parameterstointerface.py
@@ -260,9 +260,7 @@ class Root:
                 for node in staticmodbus_nodes_with_parameter_uuid
             }
 
-        lengths_equal = len(can_nodes_with_parameter_uuid) == len(
-            parameter_uuid_to_can_node
-        )
+        lengths_equal = len(can_nodes_with_parameter_uuid) >= len(parameter_uuid_to_can_node)
         if not lengths_equal:
             uuids = [u.parameter_uuid for u in can_nodes_with_parameter_uuid]
             print("\n".join(set(str(u) for u in uuids if uuids.count(u) > 1)))
